d36cf5f088bb440bf8f9ccad33dd4bd4326c40b5
fodela <particleme30@gmail.com>
2022-06-02 00:43:32 +0000

    feat: add missing fields of the artist model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Imports
 #----------------------------------------------------------------------------#
 
+from email.policy import default
 import json
 import dateutil.parser
 import babel
@@ -45,18 +46,33 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(), nullable=False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    looking_for_venues = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+# class Show(db.Model):
+  # __tablename__ = 'shows'
+  # id = db.Column(db.Integer, primary_key=True)
+  #artist_id  --> foreign for one to many btn artist and show |
+  # venue_id --> foreign key for one to many
+  # pass
 
+# class Venue(db.Model):
+#   __tablename__ = 'venues'
+  # id = db.Column(db.Integer, primary_key=True)
+  # name = db.Column(db.String())
+  # artist_id
+  # pass
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
