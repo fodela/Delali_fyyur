5d033e1f96df8ea1f334571c88ff44cff491638f
fodela <particleme30@gmail.com>
2022-06-02 00:00:57 +0000

    connect to fyyrdb in config.py

--- config.py
+++ config.py
@@ -10,4 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:test123@localhost:5432/fyyrdb'
+
+
