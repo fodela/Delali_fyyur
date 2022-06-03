d7964b47c0ac8a38f482b5bae153cd7e059028a5
fodela <particleme30@gmail.com>
2022-06-03 19:37:56 +0000

    fix: fix bug in venues/id

--- app.py
+++ app.py
@@ -30,60 +30,8 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
+from models import *
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String()), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean(), default=False)
-    seeking_description = db.Column(db.String())
-    shows = db.relationship('Show', backref='venue', lazy=True)
-    
-    def __repr__(self):
-      return f"<Venue | {self.id}  {self.name}  >"
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String()), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_venues = db.Column(db.Boolean(), default=False)
-    seeking_description = db.Column(db.String())
-    shows = db.relationship('Show', backref='artist', lazy=True)
-
-    def __repr__(self):
-      return f"<Artist | {self.id}  {self.name}  >"
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-class Show(db.Model):
-  __table__name = 'Show'
-  id = db.Column(db.Integer, primary_key=True)
-  start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-  
-  def __repr__(self):
-    return f"<Show | {self.id} venue: {self.venue_id} artist: {self.artist_id} >"
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -185,13 +133,13 @@ def show_venue(venue_id):
   # TODO: replace with real venue data from the venues table, using venue_id
   venue = Venue.query.get(venue_id)
 
-  past_shows = Venue.query.filter(Venue.start_time < datetime.now() and Venue.id == venue_id).all()
+  past_shows = Show.query.filter(Show.start_time < datetime.now(), Show.venue_id == venue_id).all()
 
-  num_past_shows = Venue.query.filter(Venue.start_time < datetime.now() and Venue.id == venue_id).count()
+  num_past_shows = Show.query.filter(Show.start_time < datetime.now(), Show.venue_id == venue_id).count()
 
-  upcoming_shows = Venue.query.filter(Venue.start_time > datetime.now() and Venue.id == venue_id).all()
+  upcoming_shows = Show.query.filter(Show.start_time > datetime.now(), Show.venue_id == venue_id).all()
 
-  num_upcoming_shows = Venue.query.filter(Venue.start_time > datetime.now() and Venue.id == venue_id).count()
+  num_upcoming_shows = Show.query.filter(Show.start_time > datetime.now(), Show.venue_id == venue_id).count()
   
 
   data = {
@@ -201,7 +149,7 @@ def show_venue(venue_id):
     'state' : venue.state,
     'address' : venue.address,
     'phone' : venue.phone,
-    'genres' : venue.genres.data,
+    'genres' : venue.genres,
     'image_link' : venue.image_link,
     'facebook_link' : venue.facebook_link,
     'website_link' : venue.website_link,
