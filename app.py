31fd08b8b51f2d03815825b9e3a344d6061b163f
fodela <particleme30@gmail.com>
2022-06-03 18:28:47 +0000

    fix: fix bug in searchw artist

--- app.py
+++ app.py
@@ -385,13 +385,27 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  search_term = request.get('search_term', '')
-  search_result = Venue.query.filter(Venue.name.ilike(f"%{search_term}%"))
+  search_term = request.form.get('search_term', '')
+  search_result = Artist.query.filter(Artist.name.ilike(f"%{search_term}%"))
+  num_result = search_result.count()
+
+
 
   response = {
-    "count": len(search_result),
-    "data": search_result
+    "Count": num_result,
+    "data" : []
   }
+
+  for artist in search_result:
+    response["data"]["id"] = artist.id
+    response["data"]["name"] = artist.name
+    response["data"]["num_upcoming_shows"] = Show.query.filter(Show.start_time > datetime.now() and Show.artist_id == artist.id).count()
+
+
+  # response = {
+  #   "count": len(search_result),
+  #   "data": search_result
+  # }
 
   # response={
   #   "count": 1,
@@ -399,7 +413,12 @@ def search_artists():
   #     "id": 4,
   #     "name": "Guns N Petals",
   #     "num_upcoming_shows": 0,
-  #   }]
+  #   },{
+  #     "id": 2,
+  #     "name": "Fo",
+  #     "num_upcoming_shows": 1,
+  #   }
+  #   ]
   # }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
