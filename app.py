fbcef888aab736c78a2c78d95e4f284d5d461af9
fodela <particleme30@gmail.com>
2022-06-03 14:50:38 +0000

    feat: add artists route

--- app.py
+++ app.py
@@ -355,16 +355,29 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data = []
+  # get all artists
+  artists = Artist.query.with_entities(Artist.id,Artist.name)
+
+  # create each artist
+  for artist in artists:
+    data.append(
+      {
+        'id': artist.id,
+        'name' : artist.name
+      }
+    )
+
+  # data=[{
+  #   "id": 4,
+  #   "name": "Guns N Petals",
+  # }, {
+  #   "id": 5,
+  #   "name": "Matt Quevedo",
+  # }, {
+  #   "id": 6,
+  #   "name": "The Wild Sax Band",
+  # }]
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
